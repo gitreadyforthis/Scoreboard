5821eadcbc7ad0f2faf7fa7abd92f89d9282a6ca
hunter <darthicer@gmail.com>
2018-05-14 16:43:54 -0400

    added tls encryption

--- src/ScoreboardServer.java
+++ src/ScoreboardServer.java
@@ -1,9 +1,10 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLServerSocket;
+import java.io.*;
 import java.net.Socket;
+import java.security.*;
+import java.security.cert.CertificateException;
 import java.util.ArrayList;
 
 
@@ -11,21 +12,27 @@ import java.util.ArrayList;
 //Questions are only specify to a game: Keep track of players  Player handler
 //Authenticate players by name
 //PLayer handler is how the player(Client) communicates with the server and the server communicates with the player(Client)
+
 /**
  * Collect all the Scoreboard Server classes in this file. Note that this file
  * MUST BE called ScoreboardServer.java
  */
 //middle man between client and game
-class ScoreboardServer{
+class ScoreboardServer {
     private ArrayList<ChallengeResponseGame> games;
-    private ArrayList<ScoreboardClient> Players;
+    private SSLContext sc;
 
-    public void startServer(int sslPort){
+    //Arraylist to hold the list of ChallengeResponseGame
+    ScoreboardServer(ArrayList<ChallengeResponseGame> games) {
+        this.games = games;
+    }
+
+    public void startServer(int sslPort) {
 
         //Creates a server socket, bound to the specified port.
-        ServerSocket server = null;
+        SSLServerSocket server = null;
         try {
-            server = new ServerSocket(sslPort);
+            server = (SSLServerSocket) sc.getServerSocketFactory().createServerSocket(sslPort);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -50,7 +57,7 @@ class ScoreboardServer{
             PrintWriter output = null;
             try {
                 if (client != null) {
-                    output = new PrintWriter(client.getOutputStream(),true);
+                    output = new PrintWriter(client.getOutputStream(), true);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -66,83 +73,56 @@ class ScoreboardServer{
 
     }
 
-    //Arraylist to hold the list of ChallengeResponseGame
-    ScoreboardServer(ArrayList<ChallengeResponseGame> games){
-        this.games = games;
-    }
-
     //setup for the ssl Connection
     //This will use the SSL with keystore and pasword
     //This will be ssl
-    public void setupSSL(String s, String password){
+    public void setupSSL(String s, String password) {
+        KeyStore ks = null;
+        try {
+            ks = KeyStore.getInstance("JKS");
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        }
+        InputStream ksIs = null;
+        try {
+            ksIs = new FileInputStream(s);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (ks != null) {
+                ks.load(ksIs, password.toCharArray());
+            }
+        } catch (IOException | NoSuchAlgorithmException | CertificateException e) {
+            e.printStackTrace();
+        }
+        KeyManagerFactory kmf = null;
+        try {
+            kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (kmf != null) {
+                kmf.init(ks, password.toCharArray());
+            }
+        } catch (KeyStoreException | UnrecoverableKeyException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
 
+        try {
+            sc = SSLContext.getInstance("TLSv1.2");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (kmf != null && sc != null) {
+
+                sc.init(kmf.getKeyManagers(), null, new SecureRandom());
+            }
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
     }
     //Start the server itself
 }
-/*
- ***ERRORS***
-User Name Selected
-Print Scoredboard
-User Name Selected
-Choosing Random Game
-What block cipher uses a block size of 128 bits?
-User Name Selected
- ***User Name is not being Saved Assume it is just a matter of program going over again***
-User Name Selected
-Choosing Random Game
-What block cipher uses a block size of 128 bits?
-0
-User Name Selected
-Choosing Random Game
-What block cipher uses a block size of 128 bits?
-0
- ***Randoming is not working or I am very Unlucky***
-User Name Selected
-Other Commands
-Expected Inputs: Help, Game, Question, Scoreboard, Random
-User Name Selected ***Keeps going back to the The top since while True?***
-Choose game Crypto or Networking
-0:Crypto /n1:Networking
-void Help(){//temp methods just for ease of understanding
-System.out.println("Expected Inputs: Help, Game, Question, Scoreboard, Random");
-}
-void randomGame(){ //temp methods just for ease of understanding
-//Random number creation for games
-Random generator = new Random();
-//Apply the number to the list of games
-ChallengeResponseGame currentGame=games.get(generator.nextInt(1));
-//add the username to the current game
-currentGame.addPlayer(UserName);
-//Question selection is done by random choice
-//Store the question for the current game by getting the questions
-//then index of the next random number to get the single question
-String question = currentGame.getQuestions().get(generator.nextInt(1)).getQuestion();
-//UserAnswer for the question
-String userAnswer = input.readLine();
-//checking the answer for the question of the current game by this.UserName
-currentGame.answer(UserName, question, userAnswer);
-}
-void Game(){//temp methods just for ease of understanding
-//The available games
-System.out.println("0:Crypto /n1:Networking");
-//Choosing the which game to play
-int gameChoice = input.readLine();
-//the currentGame is created with the games array at that array
-ChallengeResponseGame currentGame=games.get(gameChoice);
-//The available Questions
-System.out.println("Q1 or Q2");
-//Choosing the which question to play
-int gameChoice2 = input.readLine();
-//The question is choosen
-String question = currentGame.getQuestions().get(gameChoice2).getQuestion();
-//UserAnswer for the question
-String userAnswer = input.readLine();
-//checking the answer for the question of the current game by this.UserName
-currentGame.answer(UserName, question, userAnswer);
-}
-void Scoreboard(){//temp methods just for ease of understanding
-int gameChoice3 = input.readLine();
-//Should get the game at the choice of the user and return the names and scores
-games.get(gameChoice).getScores();
-}
- */
